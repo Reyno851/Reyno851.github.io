5abf1be3fc90baaeadaef7ae8f1113e804242934
unknown <reynoldchia@gmail.com>
2025-02-27 00:56:02 +0800

    Add Luffy gear showcase

---
+++ index.js
@@ -0,0 +1,4 @@
+function showImage(imageName) {
+    const imageDisplay = document.getElementById('imageDisplay');
+    imageDisplay.src = 'images/' + imageName; // Update the src to the selected image
+}
