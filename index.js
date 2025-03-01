c2f51c81d2105f36ea2a9169cf501eb46fb47239
unknown <reynoldchia@gmail.com>
2025-03-02 01:47:00 +0800

    Add tabs, add zoro images

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-function showImage(imageName) {
-    const imageDisplay = document.getElementById('imageDisplay');
-    imageDisplay.src = 'images/' + imageName; // Update the src to the selected image
+function showImage(imageName, imageDisplayElement) {
+    const imageDisplay = document.getElementById(imageDisplayElement);
+    imageDisplay.src = 'images/' + imageName; 
 }
+
+function selectStrawHat(evt, strawHatName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(strawHatName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Make JavaScript click on defaultOpen button (currently set as Luffy) on first load
+document.getElementById("defaultOpen").click();
